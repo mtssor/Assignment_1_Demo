ff60d2997ed055e7e5e4a61773a987231fc411b4
Mats <mats-sortland@hotmail.no>
2024-01-22 10:06:52 +0100

    chore(CountTo): added comment

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -6,5 +6,23 @@ class Program
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("My name is Mats");
+
+        CountTo(4);
+    }
+    
+    
+    /// <summary>
+    /// Counts from zero to the provided number
+    /// </summary>
+    /// <param name="num"></param>
+    static void CountTo(int num)
+    {
+        Console.WriteLine("Counting to" + num + "...");
+        for (int i = 1; i <= num; i++) ;
+        {
+
+        }
+
+        Console.WriteLine(num);
     }
 }
